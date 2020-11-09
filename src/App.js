8ceb45d560da6d99ddd61a452182017b0d813411
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2020-11-09 23:33:46 +0700

    bài 1 : thêm và render bảng

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  // eslint-disable-next-line no-useless-constructor
+  constructor(prop) {
+    super(prop);
+    this.state = {
+          loginInfo : {
+            name : '',
+            phone : '',
+          }
+    }
+    this.changValue = this.changValue.bind(this);
+  }
+  changValue(event){
+      // eslint-disable-next-line no-unused-vars
+     //b1 : lay state login infor
+      let loginInfo = this.state.loginInfo;
+      //b2 : lay name va gia tri cua the input
+      let name=event.target.name;
+      //b3 tham chieu den name
+       loginInfo[name]=event.target.value;
+       //b4 set lai state
+       this.setState({loginInfo: loginInfo});
+  }
+  render() {
+      let loginInfo = this.state.loginInfo;
+    return (
+        <div>
+            <input type="text" className="form-control" name="name" onChange={this.changValue} placeholder="Enter Name"/>
+          <input type="text" className="form-control" name="phone" onChange={this.changValue} placeholder="Enter Phone"/>
+            <p></p>
+            <p>
+                <span>{loginInfo.name}</span> /  <span>{loginInfo.phone}</span>
+            </p>
+        </div>
+    );
+  }
+
+
 }
 
 export default App;
